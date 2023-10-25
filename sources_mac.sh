3c1124946b903f9b4fb2a68fc18ea65b133d1939
Felipe Travi <felipe.travi@g.globo>
2023-10-25 16:50:59 -0300

    Init

---
+++ sources_mac.sh
@@ -0,0 +1,9 @@
+export WORKON_HOME=$HOME/.virtualenvs
+source /opt/homebrew/opt/virtualenvwrapper/bin/virtualenvwrapper.sh
+
+export PATH=$PATH:$HOME/.apigeecli/bin
+source /Users/felipebtravi/.apigeecli/bin
+
+
+export GOPATH="$HOME/go"
+export PATH="$GOPATH/bin:/usr/local/bin:/usr/local/bin/go:$PATH"
